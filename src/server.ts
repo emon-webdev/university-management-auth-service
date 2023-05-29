45be1c89c6460917deb10375ee13cd88e49aaa02
emon-webdev <emon.webdev@gmail.com>
2023-05-29 11:54:35 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
+import mongoose from 'mongoose'
+import app from './app'
 
-import mongoose from "mongoose";
-import app from "./app";
-
-import config from "./config";
+import config from './config'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('Database is connected successfully')
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-    } catch (error) {
-        console.log("Fail to connect database", error)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is connected successfully')
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Fail to connect database', error)
+  }
 }
 
 bootstrap()
